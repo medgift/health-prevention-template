029d78e9b7a41d86ed1df075c7cfc623cd816757
ulvbarn <hugovouillamoz@hotmail.ch>
2022-10-06 13:47:31 +0200

    First algo function for infarctus (correct version)

--- src/pages/Algorithm.js
+++ src/pages/Algorithm.js
@@ -9,25 +9,11 @@ export default class Algorithm extends React.Component{
     result = 0
 
     Infarctus(age, sex, fume, syst, dm, inf, chol, hdl,totalCHOL, eGRF,CRP) {
-        // for (var i = 0; i < arguments.length; i++) {
-        //   //  this.result += this.Calculate(this.infarctusCoeff[i], arguments[i])
-        //     this.result += this.infarctusCoeff[i] * arguments[i]
-        //     console.log(this.result)
-        // }
-
-        this.result += age * 0.0116
-        this.result += sex * 0.2148
-        this.result += fume * 0.1754
-        this.result += syst * 0.0037
-        this.result += dm * 0.2465
-        this.result += inf * 0.3399
-        this.result += chol * 0.4159
-        this.result += hdl * -0.2989
-        this.result += totalCHOL * -0.0308
-        this.result += eGRF * -0.0177
-        this.result += CRP * 0.0854
+        for (var i = 0; i < arguments.length; i++) {
+            this.result += this.infarctusCoeff[i] * arguments[i]
+            console.log(this.result)
+        }
         return this.OutputRisk(this.result)
-
     }
 
 
