34f8973136b1befe442484485660f389e5b91939
ulvbarn <hugovouillamoz@hotmail.ch>
2022-10-07 10:47:27 +0200

    display algo result

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -5,7 +5,12 @@ import { useNavigate } from "react-router-dom";
 import Algorithm from "./Algorithm";
 
 export default function Info() {
- 
 
-  return <h1><Algorithm></Algorithm></h1>;
+ var resultAlgo = new Algorithm(0,70,0,110,3.5,1.9,1,0,28,3,3,2,0,5,0,0)
+
+  return <div><h1>{resultAlgo.resultInfarctus} % of infarctus</h1>
+   <h1>{resultAlgo.resultNonInfarctus} % of non infarcuts</h1>
+  <h1>{resultAlgo.resultCancer} % of Cancer</h1>
+  <h1>{resultAlgo.resultDiabete} % of diabete</h1>
+ </div>;
 }
