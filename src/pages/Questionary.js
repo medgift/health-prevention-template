587c2b52b8069c42a47605b468d4fc806f7f3d81
ulvbarn <hugovouillamoz@hotmail.ch>
2022-10-13 11:00:17 +0200

    add calculate data in questionary class

--- src/pages/Questionary.js
+++ src/pages/Questionary.js
@@ -1,32 +1,44 @@
 import React  from "react";
 
-export default class Algorithm extends React.Component{
+export default class Questionary extends React.Component{
 
         //Data about the user
         sex = 0;
-          age= 0;
-          weight= 0;
-         height= 0;
-          syst= 0;
-          glyc= 0;
-          chol= 0;
-           diab=0;
-          inf=0;
-          avc= 0;
+        age= 0;
+        weight= 0;
+        height= 0;
+        syst= 0;
+        glyc= 0;
+        chol= 0;
+        diab=0;
+        inf=0;
+        avc= 0;
 
         //Data about his family
-          afinf=0;
-         afCancer= 0;
+        afinf=0;
+        afCancer= 0;
 
         //Data about his habits
-          smoke= 0;
-          sport= 0;
-          alcool= 0;
-          alim= 0;
+        smoke= 0;
+        sport= 0;
+        alcool= 0;
+        alim= 0;
+
+
+          //calculate Data
+        systAlgo = 0;
+        glycAlgo = 0;
+        cholAlgo = 0;
+        hdlAlgo = 0;
+        bmiAlgo = 0;
+        sportAlgo = 0;
+        alcoolAlgo = 0;
+        alimAlgo = 0;
 
     constructor() {
         super();
         }
+
 
 
         updatePersonalData(sex,age,weight,heigt,syst,glyc,chol,diab,inf,avc){
@@ -51,5 +63,15 @@ export default class Algorithm extends React.Component{
         this.alim = food;
         this.sport = sport;
         this.alcool = alcool;
+        }
+
+        calculateFinalData(){
+            this.systAlgo = this.syst == 1 ? 150 : 0;
+            this.glycAlgo = this.glyc == 1 ? 5.6 : 0;
+            this.cholAlgo = this.chol == 1 ? 5.9 : 0;
+            this.hdlAlgo = this.chol == 1 ? 0.9 : 0;
+            this.bmiAlgo = Math.pow(this.weight/this.height,2);
+
+            //TODO changement point sport etc...
         }
 }
