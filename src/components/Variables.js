73887d5086d60e0e5ebace797fdf27aff53bf388
DnPint <daniel.pintodem@hes-so.ch>
2022-10-14 15:00:53 +0200

    Global variables with starting value. questions with inputs finalized. No known bugs in questionnaire

---
+++ src/components/Variables.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//To be shared and used across the whole App
+export class Variables {
+     Poids;
+     Alcool;
+     Glyc;
+     Alim;
+     Sport;
+     Inf;
+     Gender;
+     DIAB;
+     Afcancer;
+     Avc;
+     Age;
+     Afinf;
+     Syst;
+     Fume;
+     Taille;
+
+}
