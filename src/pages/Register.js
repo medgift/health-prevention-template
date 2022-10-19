f8029cd654205d2cda1d6c24a83dfbb69510b0f1
natb1899 <natal.bumann@students.hevs.ch>
2022-10-19 13:10:00 +0200

    Save the uid with the register in the users document

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,28 +1,36 @@
 import UserForm from "../components/UserForm";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../initFirebase";
-import { useNavigate } from "react-router-dom";
+import {createUserWithEmailAndPassword, onAuthStateChanged} from "firebase/auth";
+import {auth, database} from "../initFirebase";
+import {useNavigate} from "react-router-dom";
+import {doc, setDoc} from "firebase/firestore";
 
 export default function Register() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleRegister = async (e, email, password) => {
-    e.preventDefault();
+    const handleRegister = async (e, email, password) => {
+        e.preventDefault();
 
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      navigate("/");
-    } catch (e) {
-      console.error(e);
-    }
-  };
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
 
-  return (
-      <div className="Content">
-        <div className="Login-Form">
-      <h1>Register</h1>
-      <UserForm handleSubmit={handleRegister} submitButtonLabel="Register" />
-          </div>
-    </div>
-  );
+            onAuthStateChanged(auth, async (user) => {
+                await setDoc(doc(database, "users", user.uid), {
+                    firstname: "test"
+                });
+            });
+
+            navigate("/");
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <div className="Content">
+            <div className="Login-Form">
+                <h1>Register</h1>
+                <UserForm handleSubmit={handleRegister} submitButtonLabel="Register"/>
+            </div>
+        </div>
+    );
 }
