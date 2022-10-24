26d3473f7adb6a9d66ead234a2ba6edbea588202
Pedro <pedro.saraivaf@hes-so.ch>
2022-10-24 08:59:09 +0200

    AvatarConvertingHook

--- src/pages/Customization.js
+++ src/pages/Customization.js
@@ -1,15 +1,15 @@
-// import React from "react";
-// // import AvatarCustom.js from '../components/AvatarCustom.js'
+import React from "react";
+// import AvatarCustom.js from '../components/AvatarCustom.js'
 // import  { Avatar } from '../components/AvatarCustom';
-//
-// export default class Customization extends React.Component {
-//     render(){
-//         return (
-//             <>
-//                 <h1>Choose values</h1>
-//                 <Avatar></Avatar>
-//             </>
-//         )
-//       }
-//
-// }
+
+export default class Customization extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Choose values</h1>
+                {/*<Avatar></Avatar>*/}
+            </>
+        )
+    }
+
+}
