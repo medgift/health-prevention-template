f3e23c630c652eaf9bb0101d5fd49e9924b8558d
benjamin <benjamin.biollaz@students.hevs.ch>
2022-11-04 10:47:14 +0100

    login input validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,8 +4,12 @@ import "../css/Login.css";
 import UserForm from "../components/UserForm";
 import simpleBlueBg from "./simple_blue_background.webp";
 import {useEffect} from "react";
+import {useState} from "react";
 
 export default function Login({setBackgroundImage}) {
+
+    const [loginMessage, setLoginMessage] = useState("");
+
     useEffect(()=> {
         setBackgroundImage(simpleBlueBg);
     }, []);
@@ -15,14 +19,15 @@ export default function Login({setBackgroundImage}) {
         try {
             await signInWithEmailAndPassword(auth, email, password);
         } catch (e) {
+            setLoginMessage("Incorrect email or password");
             console.error(e);
-            alert(e);
         }
     };
 
     return (
         <div className="padded_div login">
             <h2>Sign in</h2>
+            <p id="loginValidation" className="inputValidation">{loginMessage}</p>
             <UserForm handleSubmit={handleLogin} submitButtonLabel="Confirm"/>
         </div>
     );
