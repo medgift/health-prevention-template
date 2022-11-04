195498a9f1ce1cee2041cbef11d0422cd9ab1311
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-11-04 16:24:28 +0100

    Page 404

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,15 +1,20 @@
 import {Link} from "react-router-dom";
 import Login from "./Login";
 import Navbar from "../components/Navbar";
+import "../pages/404.css"
 
 export default function Page404() {
     
     return (
         <>
             <Navbar/>
-            <h2>OPS! Page Not found</h2>
+        <div id="mainC">
+            <div class="message">
+            <h2>OOPS...</h2>
             <h1>404</h1>
-            <h3>We're sorry but the page requested was not found</h3>
-        </>
+            <h3>the page you seek does not exist</h3>
+            </div>
+        </div>
+      </>
     );
 }
