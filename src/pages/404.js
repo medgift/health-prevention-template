0c62395b4e287596e298e8d4a6d4f0a600b4e090
Fabio <fabio.maiodealmeida@students.hevs.ch>
2022-11-10 19:19:40 +0100

    Code Cleanup and comments on : -404.js -EditAvatar.js -Home.js -Avatar related code in MyPage.js -Profile.js
    Signed-off-by: Fabio <fabio.maiodealmeida@students.hevs.ch>

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,11 +11,11 @@ export default function PageNotFound({setBackgroundImage}) {
         <div className={"ignore-css"}>
             <center>
                 <img src={"404_image.png"}
-                     style={{width: "30%"}}/>
+                     style={{width: "30%"}}
+                     alt={"404"}/>
                 <h3>Sadly, the page your trying to reach does not exist...</h3>
                 <p style={{fontSize: "20px"}}>PS: There is nothing at the bottom of the page...</p>
                 <div style={{marginTop: "1000px"}}/>
-                <!-- Little easter egg ;) -->
                 <img src={"https://media.tenor.com/9ud1r4sc-QQAAAAM/confused-john-travolta.gif"}
                      style={{width: "40%"}} alt={"John Travolta"}/>
                 <h4>Where did the content go ?! -Lost Travolta, 2022</h4>
